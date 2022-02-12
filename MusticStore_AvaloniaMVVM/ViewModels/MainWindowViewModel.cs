f82bf1f0dd7185cd5f8fe4266c4d683e438af45b
WizMe-M <enderman.2016@mail.ru>
2022-02-12 20:11:17 +0300

    Initial commit

---
+++ MusticStore_AvaloniaMVVM/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MusticStore_AvaloniaMVVM.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
