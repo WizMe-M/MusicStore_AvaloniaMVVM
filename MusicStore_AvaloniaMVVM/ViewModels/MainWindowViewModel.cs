1774221d95617a883347ca5327e09175f4ba6374
WizMe-M <enderman.2016@mail.ru>
2022-02-13 12:36:05 +0300

    Dark and blur window with button

--- MusicStore_AvaloniaMVVM/ViewModels/MainWindowViewModel.cs
+++ MusicStore_AvaloniaMVVM/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reactive;
 using System.Text;
+using System.Windows.Input;
 using MusicStore_AvaloniaMVVM.ViewModels;
+using ReactiveUI;
 
 namespace MusicStore_AvaloniaMVVM.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Welcome to Avalonia!";
+        public MainWindowViewModel()
+        {
+            BuyMusic = ReactiveCommand.Create(() =>
+            {
+            });
+            
+        }
+
+        private ICommand BuyMusic { get; }
     }
 }
